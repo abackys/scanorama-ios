5b9dc8df18a2ee919857b0bdd2084decfd8df1a5
Algimantas Backys <algimantas.backys@gmail.com>
2012-09-05 11:12:05 +0100

    Initial Commit

---
+++ scanorama/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  scanorama
+//
+//  Created by Lion User on 05/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
